36b1290f6e57a14cc29378d4ada949a6ef2b6de8
Elya Dzhy <elmiradzhy@gmail.com>
2021-08-25 12:11:44 +0300

    Add example of property computed

---
+++ index.js
@@ -0,0 +1,12 @@
+const familyPopulation = Number(prompt());
+
+const userFamily = {};
+
+for (let i = 0; i < familyPopulation; i++) {
+    userFamily[prompt('Name')] = {
+        age: prompt('Age'),
+        status: prompt('Status'),
+    };
+}
+
+console.log(userFamily);
